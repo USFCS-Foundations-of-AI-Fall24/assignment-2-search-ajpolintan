695befb5d0d907ca34f9b974742af376dc3925c6
ajpolintan <ajpolintan@dons.usfca.edu>
2024-09-10 23:08:56 -0700

    Updated A* implementation

--- routefinder.py
+++ routefinder.py
@@ -37,21 +37,43 @@ def a_star(start_state, heuristic_fn, goal_test, use_closed_list=True) :
     search_queue = PriorityQueue()
     closed_list = {}
     search_queue.put(start_state)
-
+    states = 0
     
+    # add starting state to closed list. CLOSED LIST IS A LIST OF STATES. NEEDS TO BE EDGES OR NODES
+    if use_closed_list : 
+        closed_list[start_state] = True
+
+    # while the priority queue is not full
     while len(search_queue) > 0 :
         next_state = search_queue.get()
-        if goal_test :
+
+        #If the goal is found
+        if goal_test(next_state) :
             return next_state
         else: 
-            edges = next_state.mars_graph.getEdges(start_state)
-            ##queue 
+            #get all the edges from the src 
+            edges = next_state.mars_graph.getEdges(next_state.location)
+            successors = [] 
+            #Check neighbors and add neighbors to sucesssors list. This appends the EDGES
             for e in edges :
-                ##
-                search_queue.put(next_state.g + heuristic_fn(e), next_state.mars_graph.getNode())
+                #create map states for every edge. Check if map state is already present
+                m = map_state(g=1, h=heuristic_fn(e), loc=e.src)
+
+                #Added cost
+                #g will always be 1 because you are always move one forward SUCESSORS IS A LIST OF EDGES
+                successors.append(m)
+
+                search_queue.put(1 + heuristic_fn(e), next_state.mars_graph.getNode())
+
+            if use_closed_list :
+                successors = [item for item in successors if item.location not in closed_list]
+                for s in successors :
+                    closed_list[s] = True
+
 
 
     ## you do the rest.
+
 
 
 ## default heuristic - we can use this to implement uniform cost search
